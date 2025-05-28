edcad7089d75de348e1b4cd0bbcbb9d84456d554
Catasi <aideecasillas@gmail.com>
2025-05-28 12:04:57 -0600

    prueba galeria

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), react()],
 });
