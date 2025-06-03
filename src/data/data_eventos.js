4ca423e9ac64b1a6579250c3983c8fcdf35d4e50
Catasi <aideecasillas14@gmail.com>
2025-06-03 17:53:00 -0600

    actualización info y estructura eventos

--- src/data/data_eventos.js
+++ src/data/data_eventos.js
@@ -1,158 +1,136 @@
 export const eventos = [
   {
-    id: 46,
     title: "VII TAXCO FILM FEST 2022",
     title_en: "VII TAXCO FILM FEST 2022",
-    subtitle: "PIAC patrocinador del VII TAXCO FILM FEST",
-    subtitle_en: "PIAC patron of the VII TAXCO FILM FEST",
+    data: "Evento con la selección final de cortometrajes en las categorías de ficción, documental, animación y experimental.",
+    data_en:
+      "Event with the final selection of cortometrajes in the categories of fiction, documentary, animation and experimental.",
     img: "/img/recuerdos/2022/VIITaxcoFilmFest.jpg",
     year: "2022",
   },
   {
-    id: 47,
     title: "Franz Mayer 2022",
     title_en: "Franz Mayer 2022",
-    subtitle:
-      "El PIAC patrocina la exposición de La fotografía a través de la mirada de Franz Mayer",
-    subtitle_en:
-      "The PIAC patronizes the exhibition of La photography through the lens of Franz Mayer",
+    data: "Exposición que permite a los visitantes acercarse a los diversos procesos fotográficos que Franz Mayer reunió como coleccionista y utilizó como fotógrafo durante 30 años. A través de cinco núcleos temáticos se presenta una selección de 133 imágenes y objetos fotográficos que ofrecen un recorrido por México y la fotografía durante la primera mitad del siglo XIX e inicios del siglo XX.",
+    data_en:
+      "Exposition that allows visitors to get acquainted with the various photographic processes that Franz Mayer gathered as a collector and used as a photographer for 30 years. Through five thematic clusters, a selection of 133 photographic images and objects is presented that offer a tour of Mexico and photography during the first half of the 19th century and the beginning of the 20th century.",
     img: "/img/recuerdos/2022/FranzMayer.jpg",
     year: "2022",
   },
   {
-    id: 48,
     title: "La excursión de las niñas muertas 2022",
     title_en: "La excursión de las niñas muertas 2022",
-    subtitle:
-      "PIAC, es orgulloso patrocinador de la puesta en escena de la obra “La excursión de las niñas muertas”",
-    subtitle_en:
-      "PIAC is pleased to sponsor the stage performance of the work “La excursión de las niñas muertas”",
+    data: "Es un relato escrito por Anna Seghers, escritora alemana y lo realizó durante su exilio en México. El relato es una reflexión hermosa y feroz en contra de los horrores de las guerras, en contra del fascismo y en contra de la muerte.",
+    data_en:
+      "This history written by Anna Seghers, a German writer, was created during her exile in Mexico. This story is a beautiful and fierce reflection against the horrors of war, against fascism and against death.",
     img: "/img/recuerdos/2022/excursion-ninas-muertas.jpg",
     year: "2022",
   },
   {
-    id: 49,
     title: "Museo Franz Mayer 2022",
     title_en: "Museum Franz Mayer 2022",
-    subtitle: "Sala de pintura y grabado",
-    subtitle_en: "Painting and drawing room",
+    data: "La muestra incluye desde los procesos más antiguos, como daguerrotipos y ambrotipos, hasta aquellos que prevalecieron en el siglo XX: impresiones plata gelatina en diversos papeles, tonos y acabados-, diapositivas de linterna mágica, vistas estereoscópicas en placas secas y fotograbados.",
+    data_en:
+      "The exhibition includes from the earliest processes, such as daguerreotypes and ambrotypes, to those that prevailed in the 20th century: plates of silver gelatin in various papers, tones and acetates-, magical diapositives, stereoscopic views on sheets of silver and photographed.",
     img: "/img/recuerdos/2022/MuseoFranzMayer.jpg",
     year: "2022",
   },
   {
-    id: 50,
     title: "25 aniversario del PIAC",
     title_en: "25th Anniversary of the PIAC",
-    subtitle:
-      "Robert Aust - Recital de Piano en el marco del 25 aniversario del PIAC",
-    subtitle_en:
-      "Robert Aust - Piano recital in the framework of the 25th anniversary of the PIAC",
+    data: "El PIAC festejará su 25 aniversario. En el marco de estos festejos, Robert Aust, talentoso pianista alemán, ofrecerá un recital de piano para homenajear la ocasión. Obras de R. Schumann, F. Chopin, J. S. Bach, G. F. Händel, F. Schubert, F. Liszt, R. Castro, M. Ponce, J. Ituarte a través de la interpretación del aclamado pianista alemán Robert Aust.",
+    data_en:
+      "PIAC will celebrate its 25th anniversary. In the context of these festivities, Robert Aust, a famous German pianist, will offer a piano recital to honor the occasion. Works of R. Schumann, F. Chopin, J. S. Bach, G. F. Händel, F. Schubert, F. Liszt, R. Castro, M. Ponce, J. Ituarte through the interpretation of the famous pianist German Robert Aust.",
     img: "/img/recuerdos/2022/PIAC25.jpg",
     year: "2022",
   },
   {
-    id: 51,
     title: "Folio de la Exposición de Amarantus 2022",
     title_en: "Folio of the Exposition of Amarantus 2022",
-    subtitle:
-      "El libro de Mariana Castillo Deball, editado por el MUAC como producto de la exposición de la artista,",
-    subtitle_en:
-      "The book of Mariana Castillo Deball, edited by the MUAC as a product of the exhibition of the artist,",
+    data: "Amarantus es la primera revisión en México del trabajo de Mariana Castillo Deball, quien, en los últimos veinte años, ha conformado un vasto cuerpo de trabajo que se inserta en los intersticios entre ciencia, narración, ficción, artes visuales y su relación con las maneras en que la historia precolonial mexicana ha sido apropiada e investigada en diferentes momentos.",
+    data_en:
+      "Amarantus is the first Mexican review of the work of Mariana Castillo Deball, who, in the last decade, has assembled a vast body of work that is inserted into the interstices between science, narration, fiction, visual arts and its relationship with the ways in which the pre-colonial Mexican historical narrative has been appropriate and investigated in different times.",
     img: "/img/recuerdos/2022/ExposiciónAmarantus.jpg",
     year: "2022",
   },
   {
-    id: 52,
     title: "Lucian Plessner 2023",
     title_en: "Lucian Plessner 2023",
-    subtitle:
-      "El guitarrista alemán Lucian Plessner cautivó con obras de compositores judío-mexicanos",
-    subtitle_en:
-      "The German guitarist Lucian Plessner caught with works of composers of the Jewish-Mexican genre",
+    data: "Dicho evento, titulado Jewish High Fidelity, se llevó a cabo en el Auditorio Silvestre Revueltasen el recinto del Inbal, con un programa que reunió obras de compositores judío-mexicanos y de autores de reconocimiento internacional.",
+    data_en:
+      "This event, titled Jewish High Fidelity, took place at the Auditorio Silvestre Revueltasen el recinto del Inbal, with a program that gathered works of Jewish-Mexican composers and internationally recognized authors.",
     img: "/img/recuerdos/2023/LucianPlessner.jpeg",
     year: "2023",
   },
   {
-    id: 53,
     title: "Tanz des Nosferatus 2023",
     title_en: "Tanz des Nosferatus 2023",
-    subtitle:
-      "Proyección en pantalla grande de la primera película de vampirxs de la historia del cine, ",
-    subtitle_en:
-      "Projection in large screen of the first film of vampires of the cinema history,",
+    data: "NOSFERATU (1922) de F. W. Murnau es una película muda alemana basada en la novela DRÁCULA de Bram Stoker. Es considerada uno de los clásicos del cine de terror y sigue siendo una obra maestra del cine expresionista.",
+    data_en:
+      "NOSFERATU (1922) of F. W. Murnau is a muddled German film based on the novel DRÁCULA of Bram Stoker. Is considered one of the classics of the expressionist cinema.",
     img: "/img/recuerdos/2023/TanzDesNosferatus.jpg",
     year: "2023",
   },
   {
-    id: 54,
     title: "El Salvaje 2023",
     title_en: "El Salvaje 2023",
-    subtitle:
-      "El dramaturgo y director David Gaitán presenta su versión de la novela El salvaje,",
-    subtitle_en:
-      "The dramaturgo and director David Gaitán presents his version of the novel El salvaje,",
+    data: "El salvaje logra llegar a la raíz de las emociones humanas y las diferentes realidades y vivencias, además, el aspecto intercultural de coproducción enriquece el diálogo en el escenario.",
+    data_en:
+      "El salvaje achieves to reach the root of human emotions and different realities and perspectives, as well as the intercultural aspect of coproduction enrich the dialogue in the stage.",
     img: "/img/recuerdos/2023/ElSalvaje.jpg",
     year: "2023",
   },
   {
-    id: 55,
     title: "Hertz Flimmern 2023",
     title_en: "Hertz Flimmern 2023",
-    subtitle:
-      "Hertzflimmern es una serie de eventos organizados por el Goethe-Institut Mexiko en donde se presentan las escenas electrónicas tanto de Alemania como de México, Centroamérica y el Caribe.",
-    subtitle_en:
-      "Hertzflimmern is a series of events organized by the Goethe-Institut Mexico where the electronic scenes of Germany and Mexico, Central America and the Caribbean are presented.",
+    data: "A través de Hertzflimmern, se crean nuevas redes en el mapa de la vanguardia musical y se promueven diálogos entre DJs, artistas, productores, clubes y aficionados a la escena electrónica.",
+    data_en:
+      "Through Hertzflimmern, new networks are created in the vanguard of musical map and dialogues are promoted between DJs, artists, producers, clubs and fans of the electronic scene.",
     img: "/img/recuerdos/2023/Hertz-Flimmern.jpg",
     year: "2023",
   },
   {
-    id: 56,
     title: "La Excursión de las niñas muertas 2024",
     title_en: "La Excursión de las niñas muertas 2024",
-    subtitle: "Un proyecto de Netty Radvanyi y Micaela Gramajo",
-    subtitle_en: "A project of Netty Radvanyi and Micaela Gramajo",
+    data: "La narradora de esta pieza escénica, Netty Radvanyi, bisnieta de Anna Seghers, y quienes forman parte del proyecto realizarán un recorrido por los espacios de una casa antigua de la colonia Santa María la Ribera para transitar por las huellas de la memoria y el destino.",
+    data_en:
+      "The narrator of this epic piece, Netty Radvanyi, great-grandaughter of Anna Seghers, and who form part of the project will take a tour of the spaces of an old house in the colony of Santa María la Ribera to travel through the traces of memory and destiny.",
     img: "/img/recuerdos/2024/LaExcursionNinasMuertas.jpg",
     year: "2024",
   },
   {
-    id: 57,
     title: "Liber Festival 2024",
     title_en: "Liber Festival 2024",
-    subtitle:
-      "El foco principal de esta edición es el largamente esperado estreno en México de Parsifal de Wagner.",
-    subtitle_en:
-      "The main focus of this edition is the long-awaited release in Mexico of Parsifal of Wagner.",
+    data: "La Novena Sinfonía de Ludwig van Beethoven es un monumento musical que simboliza la libertad y el espíritu de confraternidad entre los seres humanos.",
+    data_en:
+      "The Novena Sinfonía of Ludwig van Beethoven is a musical monument that symbolizes freedom and spirit of collaboration between human beings.",
     img: "/img/recuerdos/2024/LiberFestival.jpg",
     year: "2024",
   },
   {
-    id: 58,
     title: "Inauguración 23 FCA Festival de Cine Alemán",
     title_en: "Inauguration 23th FCA Festival of German Film 2024",
-    subtitle: "Año 2024",
-    subtitle_en: "Year 2024",
+    data: "Esta película icónica que marcó a toda una generación, que se convirtió en un filme de culto y nos enseñó una Alemania moderna, colorida, apasionada y potente, cumple 25 años de haber sido estrenada en México y lo celebramos proyectando una versión restaurada en 4K",
+    data_en:
+      "This filmic masterpiece that marked a whole generation, which turned into a cult film and taught us a modern, colored, passionate and powerful Germany, fulfilled 25 years of being broadcast in Mexico and we are proud to present a restored version in 4K",
     img: "/img/recuerdos/2024/23FCA.png",
     year: "2024",
   },
   {
-    id: 59,
     title: "EXT x VNION + BOCA BOCA 2024",
     title_en: "EXT x VNION + BOCA BOCA 2024",
-    subtitle:
-      "En alianza con el Goethe-Institut, presentan una experiencia única en la CDMX:",
-    subtitle_en:
-      "In partnership with the Goethe-Institut, present a unique experience in the CDMX:",
+    data: "Un universo de sonidos, visuales y texturas impactantes. Con un line-up de alto nivel y la participación especial de Lena Willikens, una DJ reconocida por su habilidad para explorar lo más oscuro y misterioso de la música electrónica",
+    data_en:
+      "An universe of sound, visual and texture impacting. With a line-up of high level and the participation of Lena Willikens, a DJ recognized for her ability to explore the most dark and mysterious of the electronic music",
     img: "/img/recuerdos/2024/EXT-x-VNION-BOCA-BOCA.jpg",
     year: "2024",
   },
   {
-    id: 60,
     title: "La naturaleza íntima de Ernst Saemisch 2024",
     title_en: "The naturalness of Ernst Saemisch 2024",
-    subtitle:
-      "PIAC te invita a sumergirte en la obra de Ernst Saemisch (1902-1984)",
-    subtitle_en:
-      "PIAC invites you to immerse yourself in the work of Ernst Saemisch (1902-1984)",
+    data: "El Colegio de San Ildefonso abre sus puertas a la exposición “La naturaleza íntima de la vida”, donde el color y el movimiento dialogan en cada trazo. Una oportunidad para disfrutar del arte europeo en uno de los recintos más emblemáticos de la Ciudad de México.",
+    data_en:
+      "The San Ildefonso College opens its doors to the exhibition “The Naturalness of Life”, where color and movement converse in each stroke. An opportunity to enjoy the European art in one of the most iconic rooms of the City of Mexico.",
     img: "/img/recuerdos/2024/naturaleza-piac-scaled.jpg",
     year: "2024",
   },
