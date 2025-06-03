64dc3d8a3c45697225b527ac520e6e916c09496d
Catasi <aideecasillas14@gmail.com>
2025-06-03 12:53:44 -0600

    traducciones noticias

--- src/data/data_news.js
+++ src/data/data_news.js
@@ -1,82 +1,86 @@
-
 import { generateSlug } from "../utils/generateSlug.js";
 
 export const news = [
-    {
-        slug: '',
-        title: "La Excursión de las niñas muertas 2024",
-        title_en: "La Excursión de las niñas muertas 2024",
-        subtitle: "Un proyecto de Netty Radvanyi y Micaela Gramajo",
-        subtitle_en: "A project of Netty Radvanyi and Micaela Gramajo",
-        src: "../../img/recuerdos/2024/LaExcursionNinasMuertas.jpg",
-        year: "2024",
-        link: '',
-        data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        data_en: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-        slug: '',
-        title: "Liber Festival 2024",
-        title_en: "Liber Festival 2024",
-        subtitle:
-            "El foco principal de esta edición es el largamente esperado estreno en México de Parsifal de Wagner.",
-        subtitle_en:
-            "The main focus of this edition is the long-awaited release in Mexico of Parsifal of Wagner.",
-        src: "../../img/recuerdos/2024/LiberFestival.jpg",
-        year: "2024",
-        link: 'https://liberfestival.org',
-        date: "01/01/2025",
-        place: "CDMX",
-        data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        data_en: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-        slug: '',
-        title: "Inauguración 23 FCA Festival de Cine Alemán",
-        title_en: "Inauguration 23th FCA Festival of German Film 2024",
-        subtitle: "Año 2024",
-        subtitle_en: "Year 2024",
-        src: "../../img/recuerdos/2024/23FCA.png",
-        year: "2024",
-        link: '',
-        date: "01/01/2025",
-        place: "CDMX",
-        data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        data_en: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-        slug: '',
-        title: "EXT x VNION + BOCA BOCA 2024",
-        title_en: "EXT x VNION + BOCA BOCA 2024",
-        subtitle:
-            "En alianza con el Goethe-Institut, presentan una experiencia única en la CDMX:",
-        subtitle_en:
-            "In partnership with the Goethe-Institut, present a unique experience in the CDMX:",
-        src: "../../img/recuerdos/2024/EXT-x-VNION-BOCA-BOCA.jpg",
-        year: "2024",
-        link: '',
-        date: "01/01/2025",
-        place: "CDMX",
-        data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        data_en: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-        slug: '',
-        title: "La naturaleza íntima de Ernst Saemisch 2024",
-        title_en: "The naturalness of Ernst Saemisch 2024",
-        subtitle:
-            "PIAC te invita a sumergirte en la obra de Ernst Saemisch (1902-1984)",
-        subtitle_en:
-            "PIAC invites you to immerse yourself in the work of Ernst Saemisch (1902-1984)",
-        src: "../../img/recuerdos/2024/naturaleza-piac-scaled.jpg",
-        year: "2024",
-        link: '',
-        date: "01/01/2025",
-        place: "CDMX",
-        data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        data_en: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
+  {
+    slug: "",
+    title: "La Excursión de las niñas muertas 2024",
+    title_en: "La Excursión de las niñas muertas 2024",
+    subtitle: "Un proyecto de Netty Radvanyi y Micaela Gramajo",
+    subtitle_en: "A project of Netty Radvanyi and Micaela Gramajo",
+    src: "/img/recuerdos/2024/LaExcursionNinasMuertas.jpg",
+    year: "2024",
+    link: "",
+    data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    data_en:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "",
+    title: "Liber Festival 2024",
+    title_en: "Liber Festival 2024",
+    subtitle:
+      "El foco principal de esta edición es el largamente esperado estreno en México de Parsifal de Wagner.",
+    subtitle_en:
+      "The main focus of this edition is the long-awaited release in Mexico of Parsifal of Wagner.",
+    src: "/img/recuerdos/2024/LiberFestival.jpg",
+    year: "2024",
+    link: "https://liberfestival.org",
+    date: "01/01/2025",
+    place: "CDMX",
+    data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    data_en:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "",
+    title: "Inauguración 23 FCA Festival de Cine Alemán",
+    title_en: "Inauguration 23th FCA Festival of German Film 2024",
+    subtitle: "Año 2024",
+    subtitle_en: "Year 2024",
+    src: "/img/recuerdos/2024/23FCA.png",
+    year: "2024",
+    link: "",
+    date: "01/01/2025",
+    place: "CDMX",
+    data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    data_en:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "",
+    title: "EXT x VNION + BOCA BOCA 2024",
+    title_en: "EXT x VNION + BOCA BOCA 2024",
+    subtitle:
+      "En alianza con el Goethe-Institut, presentan una experiencia única en la CDMX:",
+    subtitle_en:
+      "In partnership with the Goethe-Institut, present a unique experience in the CDMX:",
+    src: "/img/recuerdos/2024/EXT-x-VNION-BOCA-BOCA.jpg",
+    year: "2024",
+    link: "",
+    date: "01/01/2025",
+    place: "CDMX",
+    data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    data_en:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "",
+    title: "La naturaleza íntima de Ernst Saemisch 2024",
+    title_en: "The naturalness of Ernst Saemisch 2024",
+    subtitle:
+      "PIAC te invita a sumergirte en la obra de Ernst Saemisch (1902-1984)",
+    subtitle_en:
+      "PIAC invites you to immerse yourself in the work of Ernst Saemisch (1902-1984)",
+    src: "/img/recuerdos/2024/naturaleza-piac-scaled.jpg",
+    year: "2024",
+    link: "",
+    date: "01/01/2025",
+    place: "CDMX",
+    data: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    data_en:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
 ].map((item) => ({
-    ...item,
-    slug: generateSlug(item.title),
+  ...item,
+  slug: generateSlug(item.title),
 }));
