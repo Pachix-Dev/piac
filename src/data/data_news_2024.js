b3f1d485a291b930fdb8ee331be485396b46d436
Catasi <aideecasillas14@gmail.com>
2025-06-04 11:19:24 -0600

    data noticias actualizado

---
+++ src/data/data_news_2024.js
@@ -0,0 +1,111 @@
+import { generateSlug } from "../utils/generateSlug.js";
+
+export const news = [
+  {
+    slug: "",
+    width: "450px",
+    title: "La Excursión de las niñas muertas 2024",
+    title_en: "La Excursión de las niñas muertas 2024",
+    subtitle: "Un proyecto de Netty Radvanyi y Micaela Gramajo",
+    subtitle_en: "A project of Netty Radvanyi and Micaela Gramajo",
+    src: "/img/recuerdos/2024/LaExcursionNinasMuertas.jpg",
+    year: "2024",
+    date: "Sábados y Domingos | 24 feb - 17 mar",
+    date_en: "Saturdays and Sundays | 24 feb - 17 mar",
+    hour: "19 hrs",
+    hour_en: "19 hrs",
+    place: "Museo Universitario del Chopo",
+    link: "https://www.chopo.unam.mx/01ESPECIAL/artesvivas/excursion-ninas-muertas.html",
+    data: "Una colaboración de Ohtli Producción A.C., el Patronato de la Industria Alemana para la Cultura A.C. y el Museo Universitario del Chopo. <br><br> La excursión de las niñas muertas es una obra de teatro basada en un relato escrito por la autora alemana Anna Seghers en 1943, durante su exilio en México, tras recuperarse de un coma derivado de un accidente automovilístico. El texto es una reflexión contra los horrores de las guerras y el fascismo. Una pieza que dialoga, también, con las atrocidades del presente. <br><br> En un delirio febril, la autora regresa a su infancia para cumplir con una tarea postergada: la reseña de una excursión escolar en barco por el Rhin, en Mainz, su ciudad natal. El relato es un viaje de México a Alemania saltando en diferentes épocas. En 1914, nos presenta a las niñas de su clase, sus mejores amigas de infancia y maestras en el pasado, a la vez, nos lleva a su presente, en 1942, para mostrar el desenlace que tuvieron estas pequeñas en las guerras venideras y, principalmente, con la llegada del nazismo. <br><br> La narradora de esta pieza escénica, Netty Radvanyi, bisnieta de Anna Seghers, y quienes forman parte del proyecto realizarán un recorrido por los espacios de una casa antigua de la colonia Santa María la Ribera para transitar por las huellas de la memoria y el destino.",
+    data_en:
+      "A collaboration of Ohtli Production A.C., the Patronato de la Industria Alemana para la Cultura A.C. and the Museo Universitario del Chopo. <br><br> The excursion of the dead girls is a play based on an account written by the German author Anna Seghers in 1943, during his exile in Mexico, after recovering from a coma resulting from a car accident. The text is a reflection against the horrors of wars and fascism. A piece that also dialogues with the atrocities of the present. <br><br> In a febrile delirium, the author returns to her childhood to fulfill a postponed task: the review of a school boat trip on the Rhine, in Mainz, her hometown. The story is a journey from Mexico to Germany jumping at different times. In 1914, she presents the girls of her class, her best childhood friends and teachers in the past, while taking us to her present, in 1942, to show the outcome of these little ones in the coming wars and, mainly, with the advent of Nazism. <br><br> The narrator of this scenic piece, Netty Radvanyi, great-granddaughter of Anna Seghers, and those who are part of the project will make a tour through the spaces of an old house in the colony Santa María la Ribera to transit through the traces of memory and destiny.",
+  },
+  {
+    slug: "",
+    width: "400px",
+    title: "Liber Festival 2024",
+    title_en: "Liber Festival 2024",
+    subtitle:
+      "El foco principal de esta edición es el largamente esperado estreno en México de Parsifal de Wagner.",
+    subtitle_en:
+      "The main focus of this edition is the long-awaited release in Mexico of Parsifal of Wagner.",
+    src: "/img/recuerdos/2024/LiberFestival.jpg",
+    year: "2024",
+    link: "https://liberfestival.org",
+    date: "Domingo 21 de abril",
+    date_en: "Sunday 21 April",
+    hour: "17 hrs",
+    hour_en: "17 hrs",
+    cost: "$50 MXN - $100 MXN",
+    place: "Sala principal del Teatro Bicentenario | Roberto Plasencia Saldaña",
+    data: "La Novena Sinfonía de Ludwig van Beethoven es un monumento musical que simboliza la libertad y el espíritu de confraternidad entre los seres humanos. Conmemoramos el bicentenario de su estreno y en Liber Festival 2024 celebramos con un extraordinario concierto su riqueza y trascendencia musical que se ha convertido en un emblema de la lucha por la libertad y la fraternidad.",
+    data_en:
+      "The Ninth Symphony of Ludwig van Beethoven is a musical monument that symbolizes freedom and spirit of community between human beings. We remember the bicentennial of his debut and in Liber Festival 2024 we celebrate with an extraordinary concert his richness and transcendence musical that has become a symbol of the struggle for freedom and fraternity.",
+  },
+  {
+    slug: "",
+    width: "300px",
+    title: "Inauguración 23 FCA Festival de Cine Alemán",
+    title_en: "Inauguration 23th FCA Festival of German Film 2024",
+    subtitle: "Antes éramos 'Semana del Cine Alemán', ahora somos Festival",
+    subtitle_en: "Before we were 'Semana del Cine Alemán', now we are Festival",
+    src: "/img/recuerdos/2024/23FCA.png",
+    year: "2024",
+    link: "http://teatros.cultura.cdmx.gob.mx/content/inauguraci-n-del-23-festival-de-cine-alem-n",
+    date: "Martes 24 Septiembre",
+    date_en: "Monday 24 September",
+    hour: "19 hrs",
+    hour_en: "19 hrs",
+    cost: "$150 MXN - $200 MXN - $400 MXN",
+    place: "Teatro de la Cuidad, Esperanza Iris",
+    data: "Este año cambiamos y evolucionamos en una metamorfosis que, gracias a la aceptación del público, después de 23 años nos empujó a crecer y a ofrecer nuevas actividades para lxs cinéfilxs de México. Como parte de esta evolución, le dimos un pequeño giro a nuestra tradicional inauguración y presentaremos nuevamente un clásico del cine alemán, pero no de los años veinte sino de los noventa: Corre Lola, corre de Tom Tykwer. <br><br> Esta película icónica que marcó a toda una generación, que se convirtió en un filme de culto y nos enseñó una Alemania moderna, colorida, apasionada y potente, cumple 25 años de haber sido estrenada en México y lo celebraremos proyectando una versión restaurada en 4K con algunas correcciones que el mismo director realizó para conmemorar el cuarto de siglo de esta emblemática pieza del séptimo arte alemán. <br><br>Sin embargo, la tradición que nunca perderemos es la de enlazar la creación y la cinematografía alemana con la mexicana. Por ello, hemos invitado a una de las bandas de rock alternativo más respetadas de la escena independiente de este país: Hello Seahorse!, la cual reinterpretará el soundtrack de la película por medio de una creación original que nos transportará por la historia de este gran filme.",
+    data_en:
+      "This year we changed and evolved in a metamorphosis that, thanks to the acceptance of the public, after 23 years pushed us to grow and offer new activities for the cinephiles of Mexico. As part of this evolution, we have given a small twist to our traditional opening and will again present a classic of German cinema, but not from the twenties but from the nineties: Runs Lola, runs by Tom Tykwer. <br><br> This iconic film that marked an entire generation, that became a cult movie and taught us about a modern, colorful, passionate and powerful Germany, is 25 years since it was released in Mexico and we will celebrate by projecting a restored version in 4K with some corrections that the same director made to commemorate the fourth century of this emblematic piece of the German seventh art. <br><br>However, the tradition that we will never lose is to link German creation and cinematography with Mexican. Therefore, we have invited one of the most respected alternative rock bands from the independent scene in this country: Hello Seahorse! , which will reinterpret the soundtrack of the film by means of an original creation that will transport us through the history of this great film.",
+  },
+  {
+    slug: "",
+    width: "400px",
+    title: "EXT x VNION + BOCA BOCA 2024",
+    title_en: "EXT x VNION + BOCA BOCA 2024",
+    subtitle:
+      "En alianza con el Goethe-Institut, presentan una experiencia única en la CDMX:",
+    subtitle_en:
+      "In partnership with the Goethe-Institut, present a unique experience in the CDMX:",
+    src: "/img/recuerdos/2024/EXT-x-VNION-BOCA-BOCA.jpg",
+    year: "2024",
+    link: "https://web.facebook.com/events/905229515129282/",
+    date: "Viernes 13 de Diciembre",
+    date_en: "Wednesday 13 December",
+    hour: "21 hrs",
+    hour_en: "21 hrs",
+    place:
+      "Doctor Claudio Bernard 149, Doctores, 06720 Cuauhtémoc, CDMX, México",
+    data: "Hertzflimmern X EXT + VNION + BOCA BOCA en colaboración con el Goethe Institut crean una atmósfera de sonidos, visuales y texturas potentes en CDMX - con line up de alto impacto e invitada especial Lena Willikens. <br><br>Una DJ que evita los éxitos convencionales, explorando los rincones más oscuros y extraños de la música electrónica, siempre ofreciendo música impredecible y emocionante. <br><br> <strong>PROGRAMA:</strong> <ul class='columns-2 gap-4'><li>Lena Willikens</li><li>Ursula Prawn</li><li>OCTRINA (live) Octoptic + Makrina</li><li>Koscoy</li><li>DJB2B</li><li>Deejay Energy</li><li>Bluecommand</li><li>Maufo</li><li>149 FEROTO</li><li>DJ TOXIC</li></ul>",
+    data_en:
+      "Hertzflimmern X EXT + VNION + BOCA BOCA in collaboration with the Goethe-Institut, create an atmosphere of powerful sounds, visuals, and textures in Mexico City, featuring a high-impact lineup and special guest Lena Willikens. <br><br>A DJ who steers clear of conventional hits, delving into the darker and stranger corners of electronic music, always delivering unpredictable and thrilling sounds. <br><br> <strong>PROGRAM:</strong> <ul class='columns-2 gap-4'><li>Lena Willikens</li><li>Ursula Prawn</li><li>OCTRINA (live) Octoptic + Makrina</li><li>Koscoy</li><li>DJB2B</li><li>Deejay Energy</li><li>Bluecommand</li><li>Maufo</li><li>149 FEROTO</li><li>DJ TOXIC</li></ul>",
+  },
+  {
+    slug: "",
+    width: "300px",
+    title: "La naturaleza íntima de Ernst Saemisch 2024",
+    title_en: "The naturalness of Ernst Saemisch 2024",
+    subtitle:
+      "PIAC te invita a sumergirte en la obra de Ernst Saemisch (1902-1984)",
+    subtitle_en:
+      "PIAC invites you to immerse yourself in the work of Ernst Saemisch (1902-1984)",
+    src: "/img/recuerdos/2024/naturaleza-piac-scaled.jpg",
+    year: "2024",
+    link: "https://www.sanildefonso.org.mx/expos/ernstsaemisch/exposicion/index.html#:~:text=LA%20NATURALEZA%20ÍNTIMA%20DE%20LA%20VIDA&text=Su%20perspectiva%20está%20marcada%20por,de%20Weimar%2C%20conocida%20como%20Bauhaus.",
+    date: "Del 15 de diciembre de 2024 al 18 de mayo de 2025",
+    date_en: "From December 15, 2024 to May 18, 2025",
+    hour: "11 hrs - 17:30 hrs (Martes a Domingo)",
+    hour_en: "11 hrs - 17:30 hrs (Monday to Friday)",
+    place: "Justo Sierra 16 / San Ildefonso 33, Centro Histórico",
+    data: "La naturaleza íntima de Ernst Saemisch llega a México PIAC te invita a sumergirte en la obra de Ernst Saemisch (1902-1984), un artista que exploró la esencia de la naturaleza a través del expresionismo alemán.<br><br> Del 15 de diciembre de 2024 al 18 de mayo de 2025, el Colegio de San Ildefonso abre sus puertas a la exposición “La naturaleza íntima de la vida”, donde el color y el movimiento dialogan en cada trazo. <br><br>Una oportunidad para disfrutar del arte europeo en uno de los recintos más emblemáticos de la Ciudad de México.",
+    data_en:
+      "The intimate nature of Ernst Saemisch comes to Mexico PIAC invites you to immerse yourself in the work of Ernst Saemisch (1902-1984), an artist who explored the essence of nature through German expressionism. <br><br> From December 15, 2024 to May 18, 2025, the San Ildefonso College opens its doors to the exhibition “The intimate nature of life”, where color and movement dialogue in every stroke. <br><br>An opportunity to enjoy European art in one of the most emblematic venues in Mexico City.",
+  },
+].map((item) => ({
+  ...item,
+  slug: generateSlug(item.title),
+}));
