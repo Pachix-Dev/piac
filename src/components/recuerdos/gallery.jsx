53f6afa3ca4b1acd5e30a8cdf5dae76dce7d604c
Catasi <aideecasillas14@gmail.com>
2025-06-04 16:01:46 -0600

    gallery imports scss

--- src/components/recuerdos/gallery.jsx
+++ src/components/recuerdos/gallery.jsx
@@ -5,10 +5,6 @@ import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 
-// If you want you can use SCSS instead of css
-import 'lightgallery/scss/lightgallery.scss';
-import 'lightgallery/scss/lg-zoom.scss';
-
 // import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
